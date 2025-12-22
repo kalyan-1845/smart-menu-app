b76131c58ef999ae1850b4a4e3834daec88854ee
kalyan-1845 <prsnlkalyan2gmail.com>
2025-12-22 14:22:34 +0530

    fix: final meta tags and redirects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
+  resolve: {
+    alias: {
+      // This forces the app to use ONLY one version of React
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    },
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
   }
 })
