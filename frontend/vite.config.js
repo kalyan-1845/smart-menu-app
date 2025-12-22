fd5812e9f73ab9143a21935662b0012ebbecc602
kalyan-1845 <prsnlkalyan2gmail.com>
2025-12-22 11:40:42 +0530

    fix: clean install to resolve React useState error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/', // This is the fix! It tells the browser where to find your files.
 })
