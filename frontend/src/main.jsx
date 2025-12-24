ecbc47aa6c9a96a74279c0963e3742ec7a404c96
kalyan-1845 <prsnlkalyan2gmail.com>
2025-12-25 02:26:39 +0530

    LLANC

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { Toaster } from 'react-hot-toast'; // For those pro notifications
 
 // --- 🛠️ SERVICE WORKER REGISTRATION ---
