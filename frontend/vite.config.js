94e9636d1968f6d6958d5ce73f833e4efdfcf722
kalyan-1845 <prsnlkalyan2gmail.com>
2025-12-28 20:28:54 +0530

    jnjh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 This ensures assets load correctly from any URL depth
+  base: '/',
   server: {
-    host: true, // 👈 THIS ALLOWS MOBILE ACCESS
+    host: true,
     port: 5173
   },
   build: {
